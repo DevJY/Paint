d841a6578ec123d861397bb9672695137ce2aba2
DevJY <wndud5926@gmail.com>
2018-04-04 19:18:14 +0900

    initial commit

---
+++ paint/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace paint
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.square = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.mousePosition = new System.Windows.Forms.ToolStripStatusLabel();
+			this.mouseClickPosition = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "선";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button_line_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(93, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "원";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button_circle_Click);
+			// 
+			// square
+			// 
+			this.square.Location = new System.Drawing.Point(174, 12);
+			this.square.Name = "square";
+			this.square.Size = new System.Drawing.Size(75, 23);
+			this.square.TabIndex = 2;
+			this.square.Text = "사각형";
+			this.square.UseVisualStyleBackColor = true;
+			this.square.Click += new System.EventHandler(this.square_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Location = new System.Drawing.Point(12, 68);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(640, 457);
+			this.panel1.TabIndex = 3;
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+			this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseUp);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mousePosition,
+            this.mouseClickPosition});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 534);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(691, 22);
+			this.statusStrip1.TabIndex = 5;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// mousePosition
+			// 
+			this.mousePosition.Name = "mousePosition";
+			this.mousePosition.Size = new System.Drawing.Size(59, 17);
+			this.mousePosition.Text = "현재 위치";
+			// 
+			// mouseClickPosition
+			// 
+			this.mouseClickPosition.Name = "mouseClickPosition";
+			this.mouseClickPosition.Size = new System.Drawing.Size(71, 17);
+			this.mouseClickPosition.Text = "선택된 위치";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(691, 556);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.square);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Paint";
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button square;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel mousePosition;
+		private System.Windows.Forms.ToolStripStatusLabel mouseClickPosition;
+	}
+}
+
---
+++ paint/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Collections;
+
+namespace paint
+{
+
+	public enum drawMode
+	{
+		line,
+		ractangle,
+		circle,
+	};
+
+	public partial class Form1 : Form
+	{
+		
+		Point startPoint, nowPoint; // 선택한 좌표와 현재 좌표
+		Pen myPen;                  // 선
+		Brush myBrush;              // 원과 사각형
+		drawMode drawmode = drawMode.line;
+
+
+		public Form1()
+		{
+			InitializeComponent();                
+			myPen = new Pen(Color.Black);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Invalidate();
+			this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			this.SetStyle(ControlStyles.UserPaint, true);
+
+			ResizeRedraw = true;
+		}
+
+		#region 그리기모드
+
+		private void button_line_Click(object sender, EventArgs e)
+		{
+			drawmode = drawMode.line;
+			Invalidate();
+			Update();
+		}
+
+		private void button_circle_Click(object sender, EventArgs e)
+		{
+			drawmode = drawMode.circle;
+			Invalidate();
+			Update();
+		}
+
+		private void square_Click(object sender, EventArgs e)
+		{
+			drawmode = drawMode.ractangle;
+			Invalidate();
+			Update();
+		}
+
+		#endregion
+
+		private void Panel1_MouseDown(object sender, MouseEventArgs e)
+		{
+			nowPoint = new Point(e.X, e.Y);
+			startPoint = nowPoint;
+			mouseClickPosition.Text = "[" + startPoint.X + "," + startPoint.Y + "]";
+		}
+
+		private void Panel1_MouseMove(object sender, MouseEventArgs e)
+		{
+			nowPoint = new Point(e.X, e.Y);
+			mousePosition.Text = "[" + nowPoint.X + "," + nowPoint.Y + "]";
+
+			Graphics g = CreateGraphics();  // 그리기 객체 생성
+			Invalidate();
+			Update();
+			//Rectangle rect;
+
+			switch (drawmode)
+			{
+				case drawMode.line:
+					g.DrawLine(myPen, startPoint, nowPoint);
+					break;
+
+				case drawMode.circle:
+					g.DrawLine(myPen, startPoint, nowPoint);
+					break;
+
+			}
+		}
+
+		private void Panel1_MouseUp(object sender, MouseEventArgs e)
+		{
+
+		}
+
+
+
+
+
+
+	}
+}
